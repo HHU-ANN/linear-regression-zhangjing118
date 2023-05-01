753aeb84bfc29ea56775bffb4ed640fb7fe98967
unknown <208644219@qq.com>
2023-05-01 18:13:34 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -1,4 +1,5 @@
 # 最终在main函数中传入一个维度为6的numpy数组，输出预测值
+# 最终在main函数中传入一个维度为6的numpy数组，输出预测值
 
 import os
 
@@ -8,11 +9,25 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-def ridge(data):
-    pass
-    
-def lasso(data):
-    pass
+
+def ridge(x, y):
+    a = 0.5
+    E = np.eye(np.linalg.inv(np.dot(x.T, x)))
+    return np.dot(np.linalg.inv(np.dot(x, x.T) + np.dot(a, E)), np.dot(x, y))
+
+
+def lasso(x, y):
+    w = np.random.rand(1, 6)
+    a = 0.5
+    p = np.dot(x.T, y - np.dot(x.T, w))
+    z = np.dot(x.T, x)
+    if p < (-0.5 * a):
+        return (p + 0.5 * a) / z
+    elif p > (0.5 * a):
+        return (p - 0.5 * a) / z
+    else:
+        return 0
+
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
